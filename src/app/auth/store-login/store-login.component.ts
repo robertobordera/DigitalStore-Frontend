92e518746e40a12d4c90cb1363ac7638bb669778
robertobordera <robertoborderaparra@gmail.com>
2024-04-20 13:41:47 +0200

    Se añade los formularios reactivos en el registro

--- src/app/auth/store-login/store-login.component.ts
+++ src/app/auth/store-login/store-login.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StoreRegisterComponent } from '../store-register/store-register.component';
+import { AbstractControl, FormControl, NonNullableFormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-store-login',
   standalone: true,
-  imports: [StoreRegisterComponent],
+  imports: [StoreRegisterComponent,CommonModule,ReactiveFormsModule],
   templateUrl: './store-login.component.html',
   styleUrl: './store-login.component.scss'
 })
@@ -13,6 +16,8 @@ export class StoreLoginComponent implements OnInit{
   registro = false;
   login = true;
   booleanoRecibido: boolean = false;
+  #router=inject(Router);
+  #fb = inject(NonNullableFormBuilder);
 
   recibirBooleanoDelHijo(valor: boolean) {
     this.booleanoRecibido = valor;
@@ -28,13 +33,43 @@ export class StoreLoginComponent implements OnInit{
   ngOnInit(): void {
   }
 
+  email = this.#fb.control('',[
+    Validators.required,
+    Validators.email
+  ]);
+
+  password = this.#fb.control('',[
+    Validators.required,
+    Validators.minLength(4),
+  ]);
+
+  loginForm = this.#fb.group(
+    {
+      email: this.email,
+      password: this.password,
+    },
+    { validators: this.loginRequired() }
+  );
+
+  usuarioRegistro(){
+
+  }
+
   ensenarRegistro(){
     this.registro = true;
     this.login = false;
   }
 
-  
- 
+  loginRequired(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const email = control.get('email')?.value;
+      const password = control.get('password')?.value;
 
-
+      if (!email && !password) {
+        return { required: true };
+      } else {
+        return null;
+      }
+    };
+  }
 }
--- src/app/auth/store-register/store-register.component.ts
+++ src/app/auth/store-register/store-register.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { AbstractControl, FormControl, NonNullableFormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-store-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './store-register.component.html',
   styleUrl: './store-register.component.scss'
 })
 export class StoreRegisterComponent {
   @Output() enviarBooleano = new EventEmitter<boolean>();
+  #router=inject(Router);
+  #fb = inject(NonNullableFormBuilder);
 
   enviar = false
   enviarValorAlPadre() {
@@ -18,5 +23,80 @@ export class StoreRegisterComponent {
   cambiarEnviar(){
     this.enviar = true;
     this.enviarValorAlPadre();
+  }
+
+  email = this.#fb.control('',[
+    Validators.required,
+    Validators.email
+  ]);
+
+  usuario = this.#fb.control('',[
+    Validators.required
+  ]);
+
+  password1 = this.#fb.control('',[
+    Validators.required,
+    Validators.minLength(4)
+  ]);
+
+  password2 = this.#fb.control('',[
+    Validators.required,
+    Validators.minLength(4)
+  ]);
+
+  calle = this.#fb.control('',[
+    Validators.required,
+    Validators.minLength(4)
+  ]);
+
+  numeroCalle = this.#fb.control('',[
+    Validators.required
+  ]);
+
+  codigoP = this.#fb.control('',[
+    Validators.required,
+    Validators.minLength(2)
+  ]);
+
+  provincia = this.#fb.control('',[
+    Validators.required
+  ])
+
+  // avatar = this.#fb.control('',[
+  //   Validators.required,
+  //   Validators.minLength(4)
+  // ]);
+
+  formularioRegistro = this.#fb.group({
+    email:this.email,
+    usuario:this.usuario,
+    password1:this.password1,
+    password2:this.password2,
+    calle:this.calle,
+    numeroCalle:this.numeroCalle,
+    codigoP:this.codigoP,
+    provincia:this.provincia
+},{validators: this.ValidarPassword()});
+
+  usuarioRegistro(){
+
+  }
+
+  validClasses(control: FormControl, validClass: string, errorClass: string) {
+    return {
+      [validClass]: control.touched && control.valid,
+      [errorClass]: control.touched && control.invalid,
+    };
+  }
+
+  ValidarPassword():ValidatorFn{
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password1 = control.get('password1')?.value;
+      const password2 = control.get('password2')?.value;
+      if (password1 != password2) {
+        return { coinciden: true };
+      }
+      return null;      
+    };
   }
 }
